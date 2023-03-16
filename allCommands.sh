0b7d4ca0ed80a054baea32555c7cc0b95d6d27e0
hkmorpa <36601735+hkmorpa@users.noreply.github.com>
2023-03-16 16:44:47 +0530

    Add files via upload

---
+++ allCommands.sh
@@ -0,0 +1,4 @@
+command=place_order  Bprice= Sprice= quantity= Binstrument= Sinstrument=40000CE python3 kite_runner.py
+command=straddle side=sell quantity= inst1=39500PE inst2=43000CE python3 kite_runner.py
+command=cancel python3 kite_runner.py
+command=sl_runner sl_amount= python3 kite_runner.py
